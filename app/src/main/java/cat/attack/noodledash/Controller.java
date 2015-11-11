b3f48c465319668dd21ebb3b50c549eeb7902308
Kegan Hollern <ryancheek@hush.com>
2015-11-10 17:25:35 -0800

    Added FPS & Touching
    Now detects swipes & taps
    Now draws FPS (we get around 20-30 fps (we can boost this eventually)

--- app/src/main/java/cat/attack/noodledash/Controller.java
+++ app/src/main/java/cat/attack/noodledash/Controller.java
@@ -3,6 +3,8 @@ package cat.attack.noodledash;
 import android.graphics.*;
 import android.view.SurfaceHolder;
 
+import java.util.Objects;
+
 import cat.attack.noodledash.API.GameThread;
 /**
  * Created by kegan on 11/8/2015.
@@ -10,11 +12,23 @@ import cat.attack.noodledash.API.GameThread;
 public class Controller extends GameThread {
     private MainView view;
     private Paint basePaint;
-    public Player player;
+    private Paint textPaint;
+
+    private Player player;
     private long start;
     private SurfaceHolder holder;
 
-    public long execTime;
+    private double targetFPS = 60.0;
+    private long delay;
+
+
+    private long execTime;
+
+
+    public Player getPlayer()
+    {
+        return player;
+    }
 
     public Controller(MainView _view)
     {
@@ -22,24 +36,41 @@ public class Controller extends GameThread {
         player = new Player(_view);
         basePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         basePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setTextSize(60.0f);
+        textPaint.setColor(Color.RED);
+
         start = System.currentTimeMillis() + 10000;
         holder = view.getHolder();
+        delay = (long)Math.floor(1000.0 / targetFPS);
+        execTime = 0;
     }
     @Override
     protected void loop()
     {
-
+        start = System.currentTimeMillis();
         Canvas canvas = null;
         try {
             canvas = holder.lockCanvas();
             if(canvas != null)
             {
                 prepScreen(canvas);
+                drawFPS(canvas);
             }
         } finally {
             if(canvas != null)
             {
                 holder.unlockCanvasAndPost(canvas);
+            }
+        }
+        execTime = System.currentTimeMillis() - start;
+        if(execTime < delay)
+        {
+            try {
+                Thread.sleep(delay - execTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
@@ -48,5 +79,10 @@ public class Controller extends GameThread {
     {
         canvas.drawColor(Color.BLACK);
         canvas.drawBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(view.getResources(), R.drawable.test), view.getWidth(), view.getHeight(), false),0,0,basePaint);
+    }
+    private void drawFPS(Canvas canvas)
+    {
+        double FPS = Math.floor(10000.0 / execTime) / 10.0;
+        canvas.drawText("FPS: " + Objects.toString(FPS), 0, 60,textPaint);
     }
 }
--- app/src/main/java/cat/attack/noodledash/InputHandler.java
+++ app/src/main/java/cat/attack/noodledash/InputHandler.java
@@ -1,12 +1,16 @@
 package cat.attack.noodledash;
 
 import android.view.*;
+import android.widget.Toast;
 
 /**
  * Created by kegan on 11/7/2015.
  */
 public class InputHandler implements View.OnTouchListener {
-    MainView target;
+    private MainView target;
+    private float y1, y2;
+    private int MIN_SWIPE_DIST = 150;
+
     public InputHandler(MainView targetView)
     {
         target = targetView;
@@ -15,8 +19,37 @@ public class InputHandler implements View.OnTouchListener {
     public boolean onTouch(View view,MotionEvent event) {
         boolean result = false;
         if (view.equals(target)) {
+            result = true;
+            switch (event.getAction())
+            {
+                case MotionEvent.ACTION_DOWN:
+                {
+                    y1 = event.getY();
+                    break;
+                }
+                case MotionEvent.ACTION_UP:
+                {
+                    y2 = event.getY();
+                    float dy = y1 - y2;
+                    if(dy > MIN_SWIPE_DIST)
+                    {
+                        target.controller.getPlayer().jump();
+                        Toast.makeText(target.getContext(), "Swipe Up", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(-dy > MIN_SWIPE_DIST)
+                    {
+                        Toast.makeText(target.getContext(),"Swipe Down",Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(target.getContext(),"Screen Tap",Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                }
+            }
+
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                target.controller.player.jump();
+                target.controller.getPlayer().jump();
             }
         }
         return result;
